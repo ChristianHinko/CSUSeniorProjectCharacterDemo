9fd89e0df38450d88e8ae549a93d573a795a85e8
Christian Hinkle <c4hinkle@gmail.com>
2023-04-19 10:43:49 -0400

    Make empty character class.

---
+++ CharacterDemo/Source/CharacterDemo/Public/Character/CDCharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+
+#include "CDCharacter.generated.h"
+
+
+
+/**
+ * 
+ */
+UCLASS()
+class CHARACTERDEMO_API ACDCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+};
